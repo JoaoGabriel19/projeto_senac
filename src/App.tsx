67ec5e60a29d703957515c118380fb62d2b08b99
JoaoGabriel19 <gabrielsilva79@live.com>
2023-08-02 18:17:28 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import './App.css'
+import marcaSenacfecomercioSeschorizontalNegativo from './images/marcaSenacfecomercioSeschorizontalnegativo.png';
+
 
 
 function App() {
@@ -12,7 +14,7 @@ function App() {
     '&:hover': {
       backgroundColor: "#F7941D",
     },
-    
+    padding: '15px',
   };
   const textFieldCustom = {
     color: '#fff',
@@ -20,19 +22,25 @@ function App() {
     '&:hover': {
       backgroundColor: "#FFF",
     },
+    padding: '5px',
+    margin: '5px',
   };
+  const handleButtonClick = () => {
+    //Está função é chamada quando o botão é pressionado
+  }
   return (
     <>      
       <div className='telaLogin'>
         <div className='partePrincipal'>
+        <img src={marcaSenacfecomercioSeschorizontalNegativo} />
           <TextField id="login" label="Login" variant="filled" style={textFieldCustom}/>
-          <TextField id="senha" label="Senha" variant="filled" style={textFieldCustom}/>
+          <TextField id="senha" label="Senha" type="password"variant="filled" style={textFieldCustom}/>
           <p className="link">Esqueceu a senha? 
               <Link to="/alterarSenha" className="link">
                 {'Clique aqui!'}
               </Link>
           </p>
-          <Button id="realizarLogin" style={buttonStyle}>Conectar-se</Button> 
+          <Button id="realizarLogin" style={buttonStyle} onClick={handleButtonClick}>Conectar-se</Button> 
         </div>
       </div>
     </>
